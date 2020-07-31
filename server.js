b9b14477d06ac75ace550a3316f0b190f8a5cc58
moritz brandl <moritz.brandl@technikum-wien.at>
2020-07-31 15:13:56 +0200

    server: refreshToken from cookies

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ app.get("/callback", function (req, res) {
       headers: {
         Authorization:
           "Basic " +
-          new Buffer(client_id + ":" + client_secret).toString("base64"),
+          Buffer.from(client_id + ":" + client_secret).toString("base64"),
       },
       json: true,
     };
@@ -129,13 +129,13 @@ app.get("/callback", function (req, res) {
 
 app.get("/refresh_token", function (req, res) {
   // requesting access token from refresh token
-  var refresh_token = req.query.refresh_token;
+  var refresh_token = req.cookies["refreshToken"];
   var authOptions = {
     url: "https://accounts.spotify.com/api/token",
     headers: {
       Authorization:
         "Basic " +
-        new Buffer(client_id + ":" + client_secret).toString("base64"),
+        Buffer.from(client_id + ":" + client_secret).toString("base64"),
     },
     form: {
       grant_type: "refresh_token",
@@ -147,9 +147,8 @@ app.get("/refresh_token", function (req, res) {
   request.post(authOptions, function (error, response, body) {
     if (!error && response.statusCode === 200) {
       var access_token = body.access_token;
-      res.send({
-        access_token: access_token,
-      });
+      res.cookie("accessToken", access_token);
+      res.redirect("/");
     }
   });
 });
