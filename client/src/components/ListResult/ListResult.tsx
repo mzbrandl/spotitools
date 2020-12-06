a89258bd0b57c0d9c84798d3c0204ceb3ef19bb8
moritz brandl <moritz.brandl@technikum-wien.at>
2020-12-06 15:58:27 +0100

    Made generic result component

---
+++ client/src/components/ListResult/ListResult.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import * as CheckMark from "../../assets/checkmark_green.png";
+import styles from "./ListResult.module.scss";
+
+export interface IListResultProps {
+  id: string;
+  title: string;
+  cover: SpotifyApi.ImageObject;
+  author?: string | string[];
+  isChecked?: boolean;
+  handelClick(id: string, isChecked: boolean): void;
+}
+
+export const ListResult = ({
+  id,
+  title,
+  author,
+  cover,
+  isChecked,
+  handelClick,
+}: IListResultProps) => {
+  return (
+    <div
+      className={styles.listResult}
+      onClick={(_e) => handelClick(id, !isChecked)}
+    >
+      <div className={styles.overlay}>
+        <img
+          className={styles.cover}
+          src={cover?.url}
+          alt="cover"
+          height={60}
+          width={60}
+        ></img>
+        <div className={styles.textWrapper}>
+          <h5>{title}</h5>
+          {author && (
+            <div className={styles.subInfo}>
+              <p>by {author.toString()} </p>
+            </div>
+          )}
+        </div>
+        {isChecked && (
+          <img
+            className={styles.checkImg}
+            src={CheckMark.default}
+            alt="check"
+            width="20px"
+            height="20px"
+          />
+        )}
+      </div>
+    </div>
+  );
+};
